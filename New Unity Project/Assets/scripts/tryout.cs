1977bfc66e7be9122de30107e961cceb36946005
Felina-Lain <sawula@free.fr>
2016-04-20 00:24:33 +0200

    Trying shaders again, found movement, new possibilities plan for adding IA and editmode script

---
+++ New Unity Project/Assets/scripts/tryout.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tryout : MonoBehaviour {
+
+	public float start_v;
+	public float _v;
+
+	// Use this for initialization
+	void Start () {
+
+		_v = start_v;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		_v -= 0.1f * Time.deltaTime;
+		this.GetComponent<Renderer>().material.GetFloat("_Health");
+		this.GetComponent<Renderer>().material.SetFloat("_Health", _v);
+
+		if(_v < 0){_v = start_v;}
+	
+	}
+}
